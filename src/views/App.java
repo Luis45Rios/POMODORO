5d2c4f232f1d2f07dff85321fdeb3860015ba87b
Joseph Aguilar <josephadrianaguilar@gmail.com>
2024-11-22 17:54:27 -0500

    ejemplo

--- src/views/App.java
+++ src/views/App.java
@@ -1,12 +1,20 @@
 package views;
 
-import java.util.Scanner;
 import controller.functions.Validaciones;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         // @SuppressWarnings("resource");
+        int cont=100;
+        for(int i=0;i<=cont;i++){
+
+            System.out.println("La lidera no se conecta nunca a hacer el trabajo");
+            System.out.println("Steeven no sabe citar en ieee");
+            System.out.println("Luis borro el codigo y no quiere hacer nada");
+            System.out.println("Joseph hizo todo el trabajo pero luis lo borro");
+        }
         Scanner scanner = new Scanner(System.in);
         System.out.println("---------- BIENVENIDO A POMODORO ----------");
         System.out.println("Nombre de la tarea:");
@@ -14,5 +22,7 @@ public class App {
         System.out.println("Que nivel te vas a dedicar (Enfocado = 25 min, Muy Enfocado = 50 minutos):");
         String enfoque = scanner.nextLine();
         String niv_enfoque = Validaciones.ValidarEnfoque(enfoque);
+        
+        
     }
 }
