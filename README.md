5c3d3189b9f9f35ebb4d92f34c0cad5ad4f253d6
Luis <luis.h.rios@unl.edu.ec>
2024-11-27 10:17:42 -0500

    Asignación de Responsabilidades

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Pomodoro
 Es un temporizador que nos permite tener mejor control del tiempo. 
+
+# Comandos para subir los cambios
+1. git add .
+2. git commit -m "(Mensaje)"
+3. git push
+
+Joseph Aguilar(Función Reanudar (Sin Chat GPT))
+Steven Jumbo (Función Pausar)
+Domenica Rojas (Función Detener)
+Luis Ríos (Función Iniciar)
--- src/controller/functions/Validaciones.java
+++ src/controller/functions/Validaciones.java
@@ -7,8 +7,10 @@ public class Validaciones {
             for (int i = 1; i <= cont; i++) {
                 if (i  % 4 == 0) {
                     System.out.println("Te mereces un descando de 15 minutos");
+                } else if (i % 4 == 1) {
+                    System.out.println("Te mereces in descanso de 5 minutos");
                 } else {
-                    System.out.println("Temereces in descanso de 5 minutos");
+                    System.out.println("Tiempo de enfoque");
                 }
             }
         } else if (enfoque.equalsIgnoreCase("Muy enfocado"))  {
@@ -16,12 +18,12 @@ public class Validaciones {
                 if (i  % 4 == 0) {
                     System.out.println("Te mereces un descando de 30 minutos");
                 } else {
-                    System.out.println("Temereces in descanso de 10 minutos");
+                    System.out.println("Te mereces in descanso de 10 minutos");
                 }
             }
         } else {
             System.out.println("Por favor elige una opcion...");
         }
-        
+        return enfoque;
     }
 }
