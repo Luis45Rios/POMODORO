67ca160e4f79d2b184496826780663b2adf08c1c
Luis <luis.h.rios@unl.edu.ec>
2024-12-03 08:15:09 -0500

    Tiempo Ejemplo

--- src/views/App.java
+++ src/views/App.java
@@ -1,7 +1,11 @@
 package views;
 
 import controller.functions.Validaciones;
+
+import java.util.Date;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class App {
 
@@ -14,5 +18,14 @@ public class App {
         System.out.println("Que nivel te vas a dedicar (Enfocado = 25 min, Muy Enfocado = 50 minutos):");
         String enfoque = scanner.nextLine();
         String niv_enfoque = Validaciones.ValidarEnfoque(enfoque);
+
+        // Timer timer = new Timer();
+        // TimerTask tarea = new TimerTask() {
+        //     @Override
+        //     public void run() {
+        //         System.out.println("La tarea de timer se ejecuto en: " + new Date());
+        //     }
+        // };
+        // timer.schedule(tarea, 5000, 1000);
     }
 }
